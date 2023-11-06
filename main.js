9554b8f50b234469118f7006357d006e73a1ce90
Daniel <110414111+dKally@users.noreply.github.com>
2023-11-06 14:58:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -83,3 +83,14 @@ ipcMain.on('minimize-window', () => {
 const menuTemplate = [
 
 ]
+
+
+
+const NOTIFICATION_TITLE = 'Basic Notification'
+const NOTIFICATION_BODY = 'Notification from the Main process'
+
+function showNotification () {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
+
+app.whenReady().then(createWindow).then(showNotification)
